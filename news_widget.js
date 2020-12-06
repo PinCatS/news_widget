43bde2975985b64a72242d62881cafd30fcb1555
Dmitry Nosov <mizai@yandex.ru>
2020-12-06 21:50:21 +0300

    Feature: counts not readed Newsgit add -A

--- news_widget.js
+++ news_widget.js
@@ -24,19 +24,6 @@ const newsArray = [
   }
 ];
 
-function createButton() {
-  const buttonWrap = document.createElement('div');
-  const buttonText = document.createElement('p');
-  const buttonNumber = document.createElement('p');
-  buttonNumber.classList.add('widget__button-number');
-  buttonText.classList.add('widget__button-text');
-  buttonWrap.classList.add('widget__button-wrap');
-  buttonText.textContent = 'Новых новостей';
-  buttonNumber.textContent = '3';
-  buttonWrap.append(buttonText, buttonNumber);
-  return buttonWrap;
-}
-
 function createNewsExitButton() {
   const newsWrapExitButton = document.createElement('button');
   newsWrapExitButton.setAttribute('type', 'button');
@@ -51,28 +38,47 @@ function createNews(newsArray) {
   //newsItemAndArticleContainer.classList.add('widget__news-with-atricle');
   const newsArticleContainer = document.createElement('article');
   const newsDateAndAuthorContainer = document.createElement('div');
+  let numberOfNotReadedNews = document.querySelector('.widget__button-number');
 
   const newsDate = document.createElement('p');
   const newsAuthor = document.createElement('p');
   const newsParagraph = document.createElement('p');
+  const newsLink = document.createElement('a');
 
-  newsArticleContainer.classList.add('widget__article');
+  newsArticleContainer.classList.add('widget__article', 'news');
   newsDateAndAuthorContainer.classList.add('widget__news-date-author-wrap');
   newsDate.classList.add('widget__news-date');
   newsAuthor.classList.add('widget__news-author');
   newsParagraph.classList.add('widget__news-paragraph');
-
+  
+  newsItem.textContent = newsArray.title;
   newsDate.textContent = newsArray.date;
   newsAuthor.textContent = newsArray.author;
   newsParagraph.textContent = newsArray.paragraph;
+  newsLink.setAttribute('href', newsArray.link);
+  newsLink.textContent = 'подробнее...';
 
   newsItem.setAttribute('type', 'button');
   newsItem.classList.add('widget__news-item', 'news-button', 'news-button_not-visited');
-  newsItem.textContent = newsArray.title;
 
-  newsDateAndAuthorContainer.append(newsDate, newsAuthor, newsParagraph);
-  newsArticleContainer.append(newsDateAndAuthorContainer);
+  newsItem.addEventListener('click', (evt) => {
+    evt.target.classList.remove('news-button_not-visited');
+    evt.target.classList.toggle('news-button_active');
+    newsArticleContainer.classList.toggle('news_visible');
+
+    const buttonWrap = document.querySelectorAll('.widget__button-wrap');
+    [...buttonWrap].forEach(element => {
+      if(element.classList.contains('widget__button-wrap')) {
+        element.remove();
+      }
+    });
+    storedPlace.append(createButton());
+  });
+
+  newsDateAndAuthorContainer.append(newsDate, newsAuthor);
+  newsArticleContainer.append(newsDateAndAuthorContainer, newsParagraph, newsLink);
   newsItemAndArticleContainer.append(newsItem, newsArticleContainer);
+
   return newsItemAndArticleContainer;
 }
 
@@ -91,10 +97,42 @@ function formNewsList(array) {
   return listItems;
 }
 
-
-
 function renderAllElements() {
-  return storedPlace.append(allNewsContainer, createButton());
+  storedPlace.append(allNewsContainer);
+  storedPlace.append(createButton(countNotReadedNews()));
+}
+
+
+
+function countNotReadedNews() {
+  let count = 0;
+  const newsItem = document.querySelectorAll('.widget__news-item');
+  [...newsItem].forEach(element => {
+    if (element.classList.contains('news-button_not-visited')) {
+      count++;
+    }
+  });
+  if (count == 0) {
+    count = 'нет';
+  }
+  return count;
+}
+
+function createButton() {
+  const buttonWrap = document.createElement('div');
+  const buttonText = document.createElement('p');
+  const buttonNumber = document.createElement('p');
+  buttonNumber.classList.add('widget__button-number');
+  buttonText.classList.add('widget__button-text');
+  buttonWrap.classList.add('widget__button-wrap');
+  buttonText.textContent = 'Новых новостей';
+  buttonNumber.textContent = countNotReadedNews();
+  buttonWrap.append(buttonText, buttonNumber);
+  return buttonWrap;
 }
 
 renderAllElements();
+
+// let numberOfNotReadedNews = document.querySelector('.widget__button-number');
+// numberOfNotReadedNews.value = countNotReadedNews();
+// console.log(numberOfNotReadedNews.value);
