15d73e2bb695bf37e3007e0d8c9adcd749f233cc
Dmitry <mizai@yandex.ru>
2020-12-06 14:54:04 +0300

    Feature: button with js constr, in process

---
+++ news_widget.js
@@ -0,0 +1,6 @@
+function formWidgetButton() {
+  const widgetButtonTamplate = document.querySelector('#news-widget-button').content;
+  const widgetButton = widgetButtonTamplate.cloneNode(true);
+
+  
+}
